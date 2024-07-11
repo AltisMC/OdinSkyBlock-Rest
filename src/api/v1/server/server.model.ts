13a6061ce5fc90d82209689fbe6481e40fa92dbe
Efe Kurban <efekurbann@gmail.com>
2024-07-11 23:13:18 +0300

    Make spawn location nullable

--- src/api/v1/server/server.model.ts
+++ src/api/v1/server/server.model.ts
@@ -3,7 +3,7 @@ import { Schema, model } from "mongoose";
 const ServerSchema = new Schema({
   name: { type: String, required: true, unique: true },
   spawnServer: { type: Boolean, required: true },
-  spawnLocation: { type: String, required: true },
+  spawnLocation: { type: String, required: false },
   limit: { type: Number, required: true, default: 100 },
   lastPing: { type: Number, required: true, default: Date.now() },
   dead: { type: Boolean, required: true, default: false },
